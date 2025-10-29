65e0c48d105efcd2149e7c0cc9bd89873ba3f07c
MrCowdisease <brockhamilton99@outlook.com>
2025-10-28 22:16:37 -0500

    Added a script that makes enemies follow the player

---
+++ BarBeats/Assets/Scripts/Enemies/ApproachPlayer.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class EnemyChaseRigidbody2D : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 4f;
+    public float stopDistance = 0.4f;
+
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        GameObject playerObj = GameObject.FindWithTag("Player");
+
+        if (playerObj != null) 
+            target = playerObj.transform;
+        else
+            Debug.LogWarning("No GameObject with tag 'Player' found in scene.");
+    }
+
+     void Awake() => rb = GetComponent<Rigidbody2D>();
+
+    void FixedUpdate()
+    {
+        if (target == null)
+        {
+            print("test");
+            return;
+        }
+
+        Vector2 direction = (target.position - transform.position);
+        float dist = direction.magnitude;
+        if (dist > stopDistance)
+        {
+            direction.Normalize();
+            Vector2 newPos = rb.position + direction * speed * Time.fixedDeltaTime;
+            rb.MovePosition(newPos);
+        }
+    }
+
+    public void SetTarget(Transform t) => target = t;
+}
