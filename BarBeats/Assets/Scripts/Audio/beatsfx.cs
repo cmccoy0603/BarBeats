0699f49f90ede32fb04f6acd87683946c96e8b5e
b-Pepsipanda-d <esqsean@gmail.com>
2025-11-26 21:46:33 -0600

    hit sfx
    the beat hit sfx working mostly as intended though at random points it does play the wrong sfx (like the perfect will play even though you late or the late will play even though you hit it on beat) <-- will see if i can fix this slight issue since it doesn't happen all the time and seems a bit inconsistent

--- BarBeats/Assets/Scripts/Audio/beatsfx.cs
+++ BarBeats/Assets/Scripts/Audio/beatsfx.cs
@@ -6,28 +6,53 @@ public class beatsfx : MonoBehaviour
     public AK.Wwise.Bank MainBank;
     public AK.Wwise.Event play_perfect_hit;
     public AK.Wwise.Event play_late_hit;
+
+    private float lastRhythmScore = -1f; 
+    private float timeBetweenBeats = 0.5f; 
+    private float lastBeatTime = -1f; 
+    private bool isSoundTriggeredThisFrame = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         MainBank.Load();
-       
+        
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        float rhythumScore = GameManager.MusicPlayer.GetRhythmSyncScore();
+       
+            float rhythumScore = GameManager.MusicPlayer.GetRhythmSyncScore();
+            Debug.Log(rhythumScore);
 
-        if (rhythumScore > 0)
-        {
-            play_perfect_hit.Post(gameObject);
+            float currentTime = Time.time;
+
+            if (!isSoundTriggeredThisFrame)
+            {
+
+                if (rhythumScore >= .5f)
+                {
+                    play_perfect_hit.Post(gameObject);
+                    isSoundTriggeredThisFrame = true;
+                    lastBeatTime = currentTime;
+                }
+            else if (rhythumScore >= .01f && rhythumScore < .5f)
+            {
+
+                play_late_hit.Post(gameObject);
+                isSoundTriggeredThisFrame = true;
+                lastBeatTime = currentTime;
+
+            }
         }
-        if (rhythumScore <= 0) {
-        
-           play_late_hit.Post(gameObject);
+        if (currentTime - lastBeatTime >= timeBetweenBeats)
+        {
+            isSoundTriggeredThisFrame = false; 
         }
     }
+
+   
 
     private void OnDestroy()
     {
