3a6aab433db59b611ce34711ce2425abac19d3f4
MrCowdisease <brockhamilton99@outlook.com>
2025-11-29 02:33:59 -0600

    Implemented the upgrade UI and all necessary logic

--- BarBeats/Assets/Scripts/Managers/UIManager.cs
+++ BarBeats/Assets/Scripts/Managers/UIManager.cs
@@ -1,5 +1,6 @@
-using System;
 using Enums;
+using System;
+using System.Linq;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -11,7 +12,16 @@ public class UIManager : MonoBehaviour
     [SerializeField] private GameObject pauseMenu;
     [SerializeField] private GameObject settingsMenu;
 
+    [SerializeField] private GameObject upgradeMenu;
+    [SerializeField] private GameObject upgradeText1;
+    [SerializeField] private GameObject upgradeText2;
+    [SerializeField] private GameObject upgradeText3;
+
     private float upgradeGoal = 100;
+    private int upgrade1;
+    private int upgrade2;
+    private int upgrade3;
+    private int[] UpgradeOptions = {1, 2, 3, 4, 5};
 
     public void UpdateScore(float amount)
     {
@@ -48,8 +58,7 @@ public class UIManager : MonoBehaviour
         var gameState = GameManager.GameState;
         if (gameState == GameState.PLAYING)
         {
-            GameManager.GameState = GameState.PAUSED;
-            Time.timeScale = 0f;
+            PauseGame();
             pauseMenu.SetActive(true);
         }
         else if (gameState == GameState.PAUSED)
@@ -59,8 +68,7 @@ public class UIManager : MonoBehaviour
             {
                 LoadMenu();
             }
-            GameManager.GameState = GameState.PLAYING;
-            Time.timeScale = 1f;
+            UnpauseGame();
             pauseMenu.SetActive(false);
         }
     }
@@ -72,8 +80,61 @@ public class UIManager : MonoBehaviour
         settingsMenu.SetActive(!isActive);
     }
 
+    public void PauseGame()
+    {
+        GameManager.GameState = GameState.PAUSED;
+        Time.timeScale = 0f;
+    }
+
+    public void UnpauseGame()
+    {
+        GameManager.GameState = GameState.PLAYING;
+        Time.timeScale = 1f;
+    }
+
     public void UpgradePlayer()
     {
-        print("Upgrade Player");
+        PauseGame();
+
+        // This creates a clone of UpgradeOptions, shuffles it for pure random chance, and then assigns it to vars upgrade1-3.
+        int[] shuffled = UpgradeOptions;
+
+        for (int i = shuffled.Length - 1; i > 0; i--)
+        {
+            int j = UnityEngine.Random.Range(0, i + 1);
+            (shuffled[i], shuffled[j]) = (shuffled[j], shuffled[i]); // swap
+        }
+
+        upgrade1 = shuffled[0];
+        upgrade2 = shuffled[1];
+        upgrade3 = shuffled[2];
+
+
+        upgradeMenu.SetActive(true);
+    }
+
+    public void UpgradeClicked(int upgradeChoice)
+    {
+        switch(upgradeChoice)
+        {
+            case 1:
+                ApplyUpgrades(upgrade1);
+                break;
+            case 2:
+                ApplyUpgrades(upgrade2);
+                break;
+            case 3:
+                ApplyUpgrades(upgrade3);
+                break;
+            default:
+                break;
+        }    
+        UnpauseGame();
+        upgradeMenu.SetActive(false);
+    }
+
+    public void ApplyUpgrades(int chosenUpgrade)
+    {
+        return;
     }
 }
